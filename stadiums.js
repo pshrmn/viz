fd0de878e53d3d120ce46da14e9dae17388a6686
Paul Sherman <paul.sherman.88@gmail.com>
2015-04-21 00:48:50 -0500

    draw map

--- stadiums.js
+++ stadiums.js
@@ -1,131 +1,195 @@
 // approximate coordinates of team stadiums
 var teams = {
-  "49ers": {
-    "latitude": 37.4,
-    "longitude": 121.96666666666667
-  },
-  "bears": {
-    "latitude": 41.85,
-    "longitude": 87.61666666666666
-  },
-  "bengals": {
-    "latitude": 39.083333333333336,
-    "longitude": 84.5
-  },
-  "bills": {
-    "latitude": 42.766666666666666,
-    "longitude": 78.78333333333333
-  },
-  "broncos": {
-    "latitude": 39.733333333333334,
-    "longitude": 105.01666666666667
-  },
-  "browns": {
-    "latitude": 41.5,
-    "longitude": 81.68333333333334
-  },
-  "buccaneers": {
-    "latitude": 27.966666666666665,
-    "longitude": 82.5
-  },
-  "cardinals": {
-    "latitude": 33.516666666666666,
-    "longitude": 112.25
-  },
-  "chargers": {
-    "latitude": 32.766666666666666,
-    "longitude": 117.11666666666666
-  },
-  "chiefs": {
-    "latitude": 39.03333333333333,
-    "longitude": 94.48333333333333
-  },
-  "colts": {
-    "latitude": 39.75,
-    "longitude": 86.15
-  },
-  "cowboys": {
-    "latitude": 32.733333333333334,
-    "longitude": 97.08333333333333
-  },
-  "dolphins": {
-    "latitude": 25.95,
-    "longitude": 80.23333333333333
-  },
-  "eagles": {
-    "latitude": 39.9,
-    "longitude": 75.16666666666667
-  },
-  "falcons": {
-    "latitude": 33.75,
-    "longitude": 84.4
-  },
-  "giants": {
-    "latitude": 40.8,
-    "longitude": 74.06666666666666
-  },
-  "jaguars": {
-    "latitude": 30.316666666666666,
-    "longitude": 81.63333333333334
-  },
-  "jets": {
-    "latitude": 40.8,
-    "longitude": 74.06666666666666
+  "raiders": {
+    "long": -122.2,
+    "lat": 37.75
   },
   "lions": {
-    "latitude": 42.333333333333336,
-    "longitude": 83.03333333333333
-  },
-  "packers": {
-    "latitude": 44.5,
-    "longitude": 88.05
-  },
-  "panthers": {
-    "latitude": 35.21666666666667,
-    "longitude": 80.85
-  },
-  "patriots": {
-    "latitude": 42.083333333333336,
-    "longitude": 71.25
-  },
-  "raiders": {
-    "latitude": 37.75,
-    "longitude": 122.2
-  },
-  "rams": {
-    "latitude": 38.61666666666667,
-    "longitude": 90.18333333333334
-  },
-  "ravens": {
-    "latitude": 39.266666666666666,
-    "longitude": 76.61666666666666
-  },
-  "redskins": {
-    "latitude": 38.9,
-    "longitude": 76.85
+    "long": -83.03333333333333,
+    "lat": 42.333333333333336
   },
   "saints": {
-    "latitude": 29.95,
-    "longitude": 90.06666666666666
+    "long": -90.06666666666666,
+    "lat": 29.95
   },
-  "seahawks": {
-    "latitude": 47.583333333333336,
-    "longitude": 122.31666666666666
+  "bills": {
+    "long": -78.78333333333333,
+    "lat": 42.766666666666666
+  },
+  "buccaneers": {
+    "long": -82.5,
+    "lat": 27.966666666666665
+  },
+  "chargers": {
+    "long": -117.11666666666666,
+    "lat": 32.766666666666666
+  },
+  "redskins": {
+    "long": -76.85,
+    "lat": 38.9
+  },
+  "broncos": {
+    "long": -105.01666666666667,
+    "lat": 39.733333333333334
   },
   "steelers": {
-    "latitude": 40.43333333333333,
-    "longitude": 80.0
+    "long": -80.0,
+    "lat": 40.43333333333333
   },
-  "texans": {
-    "latitude": 29.683333333333334,
-    "longitude": 95.4
+  "patriots": {
+    "long": -71.25,
+    "lat": 42.083333333333336
   },
-  "titans": {
-    "latitude": 36.15,
-    "longitude": 86.76666666666667
+  "dolphins": {
+    "long": -80.23333333333333,
+    "lat": 25.95
+  },
+  "falcons": {
+    "long": -84.4,
+    "lat": 33.75
+  },
+  "cowboys": {
+    "long": -97.08333333333333,
+    "lat": 32.733333333333334
+  },
+  "packers": {
+    "long": -88.05,
+    "lat": 44.5
+  },
+  "jaguars": {
+    "long": -81.63333333333334,
+    "lat": 30.316666666666666
+  },
+  "jets": {
+    "long": -74.06666666666666,
+    "lat": 40.8
+  },
+  "rams": {
+    "long": -90.18333333333334,
+    "lat": 38.61666666666667
+  },
+  "49ers": {
+    "long": -121.96666666666667,
+    "lat": 37.4
+  },
+  "eagles": {
+    "long": -75.16666666666667,
+    "lat": 39.9
+  },
+  "bengals": {
+    "long": -84.5,
+    "lat": 39.083333333333336
   },
   "vikings": {
-    "latitude": 44.96666666666667,
-    "longitude": 93.21666666666667
+    "long": -93.21666666666667,
+    "lat": 44.96666666666667
+  },
+  "panthers": {
+    "long": -80.85,
+    "lat": 35.21666666666667
+  },
+  "ravens": {
+    "long": -76.61666666666666,
+    "lat": 39.266666666666666
+  },
+  "texans": {
+    "long": -95.4,
+    "lat": 29.683333333333334
+  },
+  "giants": {
+    "long": -74.06666666666666,
+    "lat": 40.8
+  },
+  "bears": {
+    "long": -87.61666666666666,
+    "lat": 41.85
+  },
+  "browns": {
+    "long": -81.68333333333334,
+    "lat": 41.5
+  },
+  "colts": {
+    "long": -86.15,
+    "lat": 39.75
+  },
+  "seahawks": {
+    "long": -122.31666666666666,
+    "lat": 47.583333333333336
+  },
+  "chiefs": {
+    "long": -94.48333333333333,
+    "lat": 39.03333333333333
+  },
+  "titans": {
+    "long": -86.76666666666667,
+    "lat": 36.15
+  },
+  "cardinals": {
+    "long": -112.25,
+    "lat": 33.516666666666666
   }
+}
+
+function drawMap(map){
+    var mapData = topojson.feature(map, map.objects.states).features;
+
+    var width = 960;
+    var height = 600;
+    var scale = 1200;
+    var margin = {
+        top: 75,
+        right: 75,
+        bottom: 75,
+        left: 75
+    };
+
+    var projection = d3.geo.albersUsa()
+        .scale(scale)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    var svg = d3.select('body').append('svg')
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+    
+    var country = svg.append("g")
+        .datum(mapData)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .classed("country", true);
+
+    var states = country.append("g").selectAll("g.state")
+            .data(function(d){ return d;})
+        .enter().append("g")
+            .classed("state", true)
+            .append("path")
+                .classed("outline", true)
+                .attr("d", path);
+
+    // eastern most longitude: 66.95
+    // western most longitude: 124.78
+    // southern most latitude: 24.44
+    // northern most latitude: 49.38
+
+    var teamsArray = [];
+    for ( var key in teams ) {
+        var obj = teams[key];
+        obj.name = key;
+        teamsArray.push(obj);
+    }
+
+    country.append("g").selectAll("g.team")
+            .data(teamsArray)
+        .enter().append("g")
+            .classed("team", true)
+            .attr("transform", function(d){
+                var coords = projection([d.long, d.lat]);
+                return "translate(" + coords[0] + "," + coords[1] + ")";
+            })
+            .attr("title", function(d){
+                return d.name;
+            })
+            .append("circle")
+                .attr("r", 5)
 }
